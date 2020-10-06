f7c0286fd37a84d8d8b23220994912eb5e4cfa2c
Pedro Soares Ferraz <peusoaresf@outlook.com>
2020-10-05 21:24:56 -0300

    Ajusta tamanho máximo de uma mensagem no chat

--- src/components/ChatMessage.js
+++ src/components/ChatMessage.js
@@ -4,37 +4,38 @@ import { useRecoilValue } from 'recoil';
 import { user as userAtom } from '../atoms/user.js';
 
 export default function ChatMessage({ message }) {
-    const user = useRecoilValue(userAtom);
+  const user = useRecoilValue(userAtom);
 
-    const isOutgoing = () => message.sentBy === user.id;
+  const isOutgoing = () => message.sentBy === user.id;
 
-    return (
-        <View style={[styles.messageContainer, isOutgoing() ? styles.outgoingMessage : styles.incomingMessage]}>
-            <Text style={[styles.messageText, isOutgoing() ? styles.outgoingMessageText : styles.incomingMessageText]}>{message.text}</Text>
-        </View>
-    );
+  return (
+    <View style={[styles.messageContainer, isOutgoing() ? styles.outgoingMessage : styles.incomingMessage]}>
+      <View style={{ maxWidth: '85%' }}>
+        <Text style={[styles.messageText, isOutgoing() ? styles.outgoingMessageText : styles.incomingMessageText]}>{message.text}</Text>
+      </View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    messageContainer: {
-        flex: 1,
-        flexDirection: 'row',
-        margin: 10
-    },
-    incomingMessage: {
-        justifyContent: 'flex-start'
-    },
-    outgoingMessage: {
-        justifyContent: 'flex-end'
-    },
-    messageText: {
-        borderRadius: 10,
-        padding: 10
-    },
-    outgoingMessageText: {
-        backgroundColor: '#DCF8C6'
-    },
-    incomingMessageText: {
-        backgroundColor: '#FFFFFF'
-    }
+  messageContainer: {
+    flexDirection: 'row',
+    margin: 10
+  },
+  incomingMessage: {
+    justifyContent: 'flex-start'
+  },
+  outgoingMessage: {
+    justifyContent: 'flex-end'
+  },
+  messageText: {
+    borderRadius: 10,
+    padding: 10
+  },
+  outgoingMessageText: {
+    backgroundColor: '#DCF8C6'
+  },
+  incomingMessageText: {
+    backgroundColor: '#FFFFFF'
+  }
 });
